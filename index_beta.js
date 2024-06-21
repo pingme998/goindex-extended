f4facadcff5041a5f924efa3c3150dfdf11029a1
pingme998 <77038591+pingme998@users.noreply.github.com>
2024-06-21 15:46:14 +0530

    just for personal use

--- index_beta.js
+++ index_beta.js
@@ -1,12 +1,12 @@
 const authConfig = {
   "siteName": "GoIndex Extended", // WebSite Name
-  "siteIcon": "https://raw.githubusercontent.com/cheems/goindex-extended/master/images/favicon-x.png", //or fevicon-x-light.png
+  "siteIcon": "https://raw.githubusercontent.com/pingme998/goindex-extended/master/images/favicon-x.png", //or fevicon-x-light.png
   "version": "1.4.1", // VersionControl, do not modify manually
   // client_id & client_secret - PLEASE USE YOUR OWN!
   "client_id": "", // Client ID
   "client_secret": "", // Client Secret
   "refresh_token": "", // Refresh Token
-  "folder_list_url": "", // Folder Id List(Only if you have any folders in roots) - Sample: https://cdn.jsdelivr.net/gh/cheems/goindex-extended@master/sample-files/sample-folder-id-list.txt
+  "folder_list_url": "", // Folder Id List(Only if you have any folders in roots) - Sample: https://cdn.jsdelivr.net/gh/pingme998/goindex-extended@master/sample-files/sample-folder-id-list.txt
   // Crypt Secret must be 32 characters long - Sample: "1234567890123456abcdefghIJKLMN!*" | don't use these characters (\,/,",') 
   "crypt_secret": "", // Crypt Secret (Required)* This is used to encrypt file ids
   /**
@@ -219,7 +219,7 @@ function html(current_drive_order = 0, model = {}) {
     window.UI = JSON.parse('${JSON.stringify(uiConfig)}');
     window.PLYR = JSON.parse('${JSON.stringify(playerConfig)}');
   </script>
-  <script src="//cdn.jsdelivr.net/gh/cheems/goindex-extended@4d1427558a21603e7c7e7dc986a154a9560f4835/app_beta.js"></script>
+  <script src="//cdn.jsdelivr.net/gh/pingme998/goindex-extended@4d1427558a21603e7c7e7dc986a154a9560f4835/app_beta.js"></script>
 </head>
 <body>
 </body>
