49880182e37e0587049a9b2a53605943ca38b9d8
pingme998 <77038591+pingme998@users.noreply.github.com>
2024-06-21 16:09:45 +0530

    for personal use only

--- app.js
+++ app.js
@@ -5487,30 +5487,30 @@ if (UI.dark_mode) {
 }
 if (UI.dark_mode) {
   document.write(
-    '<link rel="stylesheet" href="//rawcdn.githack.com/pingme998/goindex-extended/b46bb643bb1c1314b669142e5e62d96a877b952b/css/mdui_v2_dark.min.css">'
+    '<link rel="stylesheet" href="//rawcdn.githack.com/pingme998/goindex-extended/master/css/mdui_v2_dark.min.css">'
   );
 } else {
   document.write(
-    '<link rel="stylesheet" href="//rawcdn.githack.com/pingme998/goindex-extended/b46bb643bb1c1314b669142e5e62d96a877b952b/css/mdui_v2.min.css">'
+    '<link rel="stylesheet" href="//rawcdn.githack.com/pingme998/goindex-extended/master/css/mdui_v2.min.css">'
   );
 }
 if (UI.dark_mode) {
   document.write(
-    '<link rel="stylesheet" href="//rawcdn.githack.com/pingme998/goindex-extended/f9af6b2192974a4a9c1b222efed8d29ca6cd1927/css/nexmoe_v2_dark.min.css">'
+    '<link rel="stylesheet" href="//rawcdn.githack.com/pingme998/goindex-extended/master/css/nexmoe_v2_dark.min.css">'
   );
 } else {
   document.write(
-    '<link rel="stylesheet" href="//rawcdn.githack.com/pingme998/goindex-extended/f9af6b2192974a4a9c1b222efed8d29ca6cd1927/css/nexmoe_v2.min.css">'
+    '<link rel="stylesheet" href="//rawcdn.githack.com/pingme998/goindex-extended/master/css/nexmoe_v2.min.css">'
   );
 }
 document.write(
-  '<script src="//rawcdn.githack.com/pingme998/goindex-extended/295ceaf2d64b2cb8578b21c0313d75b7bc8738a1/js/mdui.min.js"></script>'
+  '<script src="//rawcdn.githack.com/pingme998/goindex-extended/master/js/mdui.min.js"></script>'
 );
 document.write(
-  '<script src="//rawcdn.githack.com/pingme998/goindex-extended/295ceaf2d64b2cb8578b21c0313d75b7bc8738a1/js/flv.min.js"></script>'
+  '<script src="//rawcdn.githack.com/pingme998/goindex-extended/master/js/flv.min.js"></script>'
 );
 document.write(
-  '<script src="//rawcdn.githack.com/pingme998/goindex-extended/295ceaf2d64b2cb8578b21c0313d75b7bc8738a1/js/DPlayer.min.js"></script>'
+  '<script src="//rawcdn.githack.com/pingme998/goindex-extended/master/js/DPlayer.min.js"></script>'
 );
 document.write(
   '<script src="//cdn.jsdelivr.net/npm/marked@4.0.0/marked.min.js"></script>'
@@ -5583,9 +5583,8 @@ function init() {
   if (UI.credits) {
     html += `
     <div style="margin-bottom: 20px" class="mdui-center mdui-text-center">
-      <a href="https://github.com/pingme998/goindex-extended" target="_blank">
-        <img src="https://img.shields.io/static/v1?label=pingme998&amp;message=goindex-extended&amp;color=1E89F2&amp;labelColor=374455&amp;logo=github" alt="pingme998 - goindex-extended">
-      </a>
+      <a 
+   </a>
     </div>
     </div>
     `
